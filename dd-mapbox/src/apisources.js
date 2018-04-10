c2997ddce682895e4d7d26c501d5f1993475f173
Adriaanse <bart.adriaanse@gmail.com>
2018-04-10 15:19:14 +0200

    remove fews api source

--- dd-mapbox/src/apisources.js
+++ dd-mapbox/src/apisources.js
@@ -6,12 +6,7 @@ const apisources = [
   {
     'id': 'lizard',
     'baseurl': 'https://hhnk.lizard.net/dd/api/v1'
-  },
-  {
-    'id': 'fews',
-    'baseurl': 'http://tl-tc097.xtr.deltares.nl:8080/FewsWebServices/rest/digitaledelta/v1/'
-  }
-]
+  }]
 
 export {
   apisources
